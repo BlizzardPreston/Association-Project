ca3b5e7b7e371b999d44667b13d306c21f61ff27
XinjianCHEN <1478337513@qq.com>
2022-08-18 11:43:26 +0800

    启动文件增加日志

--- Association-Project/College_project/src/main/java/com/association/College_project/AssociationApplication.java
+++ Association-Project/College_project/src/main/java/com/association/College_project/AssociationApplication.java
@@ -1,27 +1,25 @@
 package com.association.College_project;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
-
+@Slf4j
 @SpringBootApplication(scanBasePackages = "com.association")
-@MapperScan("com.association.common.Dao")
+@MapperScan("com.association")
 @EnableCaching
 
 public class AssociationApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(AssociationApplication.class, args);
         SpringApplication.run(AssociationApplication.class, args);
+        log.info("------------------------------AssociationApplication 启动完毕-----------------------------------");
     }
-
-//    public void first() {
-//        PrimaryRedisProperties primaryRedisProperties=new PrimaryRedisProperties();
-//    }
 
     @Bean
     public RestTemplate restTemplate() {
